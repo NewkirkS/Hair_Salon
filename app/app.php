a917d1e9ce333a9b638e551a81f6320c7cdf0a5b
sn <newkirk771@gmail.com>
2016-09-23 14:47:11 -0700

    Set up get and post routes for stylist page. Tested and working.

--- app/app.php
+++ app/app.php
@@ -1,18 +1,67 @@
 <?php
+    date_default_timezone_set('America/Los_Angeles');
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/Client.php";
     require_once __DIR__."/../src/Stylist.php";
 
-    $server = 'mysql:host=localhost:8889;dbname=hair_salon'; //Check port for localhost in mamp
+    $app = new Silex\Application();
+
+    $server = 'mysql:host=localhost;dbname=hair_salon'; //Check port for localhost in mamp
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    $app = new Silex\Application();
-
     $app->register(new Silex\Provider\TwigServiceProvider(), array(
         'twig.path' => __DIR__.'/../views'
     ));
+
+    //STYLIST ROUTES
+
+    //home, includes form for creating stylists and lists stylists
+
+    $app->get("/", function() use ($app) {
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    // post route for creating a stylist, goes back to index
+
+    $app->post("/create_stylist", function() use ($app) {
+        $new_stylist = new Stylist($_POST['name']);
+        $new_stylist->save();
+        return $app['twig']->render('stylists.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    //get route for going to update page for particular stylist
+
+
+
+    //update route to change stylist, goes back to index
+
+
+
+    //delete route for deleting a single stylist, goes back to index
+
+
+
+    //CLIENT ROUTES
+
+    //get route for displaying all clients of a particular stylist, goes to clients page
+
+
+
+    //post route for adding a client, goes to client page
+
+
+
+    //get route for going to update client page
+
+
+
+    //update route for updating client, goes back to client page
+
+
+
+    //delete route for individual client, goes back to client page
 
 
 
