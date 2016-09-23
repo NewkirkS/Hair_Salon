6ed678e77d65cfec1e824ed8ffd28b2eb646a1a6
sn <newkirk771@gmail.com>
2016-09-23 09:50:17 -0700

    Pass stylist test: setName

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -33,15 +33,29 @@
         function test_getName()
         {
             //Arrange
-            $id = 1;
             $name = "Jim";
-            $test_stylist = new Stylist($name, $id);
+            $test_stylist = new Stylist($name);
 
             //Act
             $result = $test_stylist->getName();
 
             //Assert
             $this->assertEquals($name, $result);
+        }
+
+        function test_setName()
+        {
+            //Arrange
+            $name = "Jim";
+            $test_stylist = new Stylist($name);
+
+            $new_name = "Phil";
+
+            //Act
+            $test_stylist->setName($new_name);
+
+            //Assert
+            $this->assertEquals($new_name, $test_stylist->getName());
         }
 
     }
